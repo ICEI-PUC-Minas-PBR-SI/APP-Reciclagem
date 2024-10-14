83e1149b3e73a3f02da9db1f7b4897765340d2f1
joao.mendes <joao.mendes@dysrup.com.br>
2024-10-14 18:58:30 -0300

    feat: criado script para popular a tabela de profile e colocado email, username para ser unique

---
+++ src/api/prisma/migrations/20241014172615_script_insert_profiles/migration.sql
@@ -0,0 +1,5 @@
+INSERT INTO app_reciclagem.perfil (id, name, label, is_admin, status)
+VALUES 
+(1, 'Administrator', 'ADMINISTRATOR', 1, 1),
+(2, 'Coletor', 'COLLECTOR', 0, 1),
+(3, 'Cliente', 'CLIENT', 0, 1);
